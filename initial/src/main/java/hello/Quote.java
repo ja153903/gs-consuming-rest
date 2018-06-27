54e6fc185e91fde57af180da92dcb56ba5610102
jaimeabbariao <jaimeabbariao@gmail.com>
2018-06-26 22:34:02 -0400

    spring application

---
+++ initial/src/main/java/hello/Quote.java
@@ -0,0 +1,39 @@
+package hello;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+// if property from json is not known to class
+// we can ignore it.
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Quote {
+
+    private String type;
+    private Value value;
+
+    public Quote() {
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "type='" + type + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
